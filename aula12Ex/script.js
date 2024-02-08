c4bf2332c6ad7a9ec7244a9d4030b01ae6bdb181
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-02-08 17:31:04 -0300

    fix: local de imagens

---
+++ aula12Ex/script.js
@@ -0,0 +1,15 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('img')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 12
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora > 0 || hora < 12 ){
+        img.src = 'manha.jpg'
+    } else if (hora < 18){
+        img.src = 'tarde.jpg'
+    } else {
+        img.src = 'noite.jpg'
+    }
+}
