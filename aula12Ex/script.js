d05c4769227569de58fa0a37e275df5f1f30af3c
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-02-20 21:13:28 -0300

    fix: tag de img

--- aula12Ex/script.js
+++ aula12Ex/script.js
@@ -1,11 +1,11 @@
 function carregar(){
     var msg = window.document.getElementById('msg')
-    var foto = window.document.getElementById('imagem')
+    var foto = window.document.getElementById('img')
     var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas.`
+    var hora = 
+    //var hora = data.getHours()
+    msg.innerHTML = `<strong>Agora são ${hora} horas.</strong>`
     if (hora > 0 && hora < 12){
-        msg.innerHTML = 'Tá certo, img abaixo:';
         foto.src = ('manha.png')
     } else if (hora < 18){
         foto.src = ('tarde.png')
