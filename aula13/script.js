889246d504922f4407777ae924f19c809f84e4b6
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-02-24 19:45:18 -0300

    feat: Descrobrindo sexo do usuario através do if

--- aula13/script.js
+++ aula13/script.js
@@ -8,6 +8,14 @@ function verificar () {
     if (nascimento_usuario.length == 0 || nascimento_usuario > ano_atual){
         window.alert ('Tem algo de errado, tente novamente.')
     } else {
-        window.alert('Tudo certo')
+        var sexo_escolhido = window.document.getElementsByName('sexo')
+        var idade = ano_atual - nascimento_usuario
+        var genero = ''
+        if (sexo_escolhido[0].checked){
+            genero = 'homem'     
+        } else if (sexo_escolhido[1].checked) {
+            genero = 'Mulher'
+        }
+        resposta.innerHTML = `Detectamos um(a) ${genero} com ${idade} anos de idade.`
     }
 }
