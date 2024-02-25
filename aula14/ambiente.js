d0bae5a913dcb3d40b4a0fb68e139948512029e1
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-02-25 12:40:52 -0300

    feat: do while

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -1,12 +1,20 @@
 var contador = 1
+do {
+    console.log(`Tudo bem! Esse é "tudo bem" de número ${contador}`)
+    contador++
+} while (contador <= 6)
+
+/* 
+var contador = 1
 while (contador <= 300){
     console.log(`Tudo bem! Esse é "tudo bem" de número ${contador}`)
     contador++
-}
+} 
+*/
 
 /*console.log("Tudo bem?")
 console.log("Tudo bem?")
-console.log("Tudo bem?")
+console.log("Tudo bem?") 
 console.log("Tudo bem?")
 console.log("Tudo bem?")
 console.log("Tudo bem?")
