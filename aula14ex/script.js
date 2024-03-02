d5f7a72e6083a8d72517324ad31ff3502cf607e2
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-03-02 14:48:21 -0300

    feat: Todos os erros possiveis que o usuario pode cometer

--- aula14ex/script.js
+++ aula14ex/script.js
@@ -3,9 +3,19 @@ function contar(){
     var n_final = window.document.getElementById ('n_final').value
     var intervalo = window.document.getElementById ('intervalo').value
     var resposta = window.document.getElementById ('resposta')
+    var calculo = ''
 
-    while (n_inicial <= n_final){
-        var n_inicial = n_inicial + intervalo
-        
+    if (n_inicial.length == 0 || n_final.length == 0){
+        resposta.innerHTML = ("Impossivel contar. <br> Defina um número para o começo e outro para o final.")
+    } else if (intervalo.length == 0){
+        resposta.innerHTML = ("Impossivel contar. <br> Defina uma quantidade de passos.")
+    } else if (intervalo == 0){
+        window.alert ("Intervalo '0' é invalido, por isso consideraremos intervalo de um em um")
+        var intervalo = 1
     }
+
+    /*while (n_inicial <= n_final){
+        var n_inicial = n_inicial + intervalo
+
+    }*/
 }
