dce3af70f65ff142f04d41772f1cc72b1d203e9f
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-03-02 14:33:46 -0300

    feat: definindo todas as variaveis no js

--- aula14ex/script.js
+++ aula14ex/script.js
@@ -1,3 +1,11 @@
 function contar(){
-    
+    var n_inicial = window.document.getElementById ('n_inicial').value
+    var n_final = window.document.getElementById ('n_final').value
+    var intervalo = window.document.getElementById ('intervalo').value
+    var resposta = window.document.getElementById ('resposta')
+
+    while (n_inicial <= n_final){
+        var n_inicial = n_inicial + intervalo
+        
+    }
 }
