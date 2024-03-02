befec62f7e3e26b2778499e4f6aa2cb022382eda
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-03-02 17:24:20 -0300

    fix: tentando resolver um bug na linha 21

--- aula14ex/script.js
+++ aula14ex/script.js
@@ -3,8 +3,12 @@ function contar(){
     var n_final = window.document.getElementById ('n_final').value
     var intervalo = window.document.getElementById ('intervalo').value
     var resposta = window.document.getElementById ('resposta')
-    var calculo = ''
+    var numero_calculado = ''
+    var numero_resposta = ''
 
+    window.alert (`Inicial = ${n_inicial}`)
+    window.alert (`Final = ${n_final}`)
+    window.alert (`Intervalo = ${intervalo}`)
     if (n_inicial.length == 0 || n_final.length == 0){
         resposta.innerHTML = ("Impossivel contar. <br> Defina um número para o começo e outro para o final.")
     } else if (intervalo.length == 0){
@@ -12,10 +16,23 @@ function contar(){
     } else if (intervalo == 0){
         window.alert ("Intervalo '0' é invalido, por isso consideraremos intervalo de um em um")
         var intervalo = 1
+    } else if (n_inicial > n_final) {
+        resposta.innerHTML = ("{ERRO} Numero inicial maior que numero final.")
+    } else if (intervalo > n_final) {
+        resposta.innerHTML = (`{ERRO} Número de passos não comportado. Observe:  ${intervalo} > ${n_final}`)
+    } else {
+        resposta.innerHTML = ('Agora foi')
     }
-
-    /*while (n_inicial <= n_final){
-        var n_inicial = n_inicial + intervalo
-
-    }*/
 }
+
+
+/*
+        do {
+            var numero_calculado = n_inicial 
+            if (numero_calculado != n_inicial){
+                var numero_resposta = n_inicial + '' + n_calculado
+            }
+            var numero_resposta = n_inicial + '' + n_calculado
+            var numero_calculado =  numero_calculado + intervalo
+        } while (numero_calculado <= n_final);
+        */
