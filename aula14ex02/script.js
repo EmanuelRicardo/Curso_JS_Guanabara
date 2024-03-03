f5989f1d3d88ef812c8ef48f5758bba914d38959
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-03-03 13:52:19 -0300

    feat: definindo as variaveis e erros do programa

--- aula14ex02/script.js
+++ aula14ex02/script.js
@@ -1,3 +1,10 @@
 function gerar(){
-    
+    var n_escolhido = window.document.getElementById ('n_escolhido').value
+
+    if (n_escolhido.length == 0){
+        window.alert (`[ERRO] Faltou escolher um número para a tabuada`)
+    } else {
+        window.alert (`Tudo ok até aqui`)
+    }
 }
+
