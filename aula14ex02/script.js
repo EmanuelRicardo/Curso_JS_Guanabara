eb3536d195f4badb98e949f79736ac9299a84ebe
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-03-03 17:45:54 -0300

    feat: js lançar tabuada na tela

--- aula14ex02/script.js
+++ aula14ex02/script.js
@@ -1,10 +1,16 @@
 function gerar(){
     var n_escolhido = window.document.getElementById ('n_escolhido').value
+    var tabuada_area = window.document.getElementById ('tabuada_area')
 
     if (n_escolhido.length == 0){
         window.alert (`[ERRO] Faltou escolher um número para a tabuada`)
     } else {
-        window.alert (`Tudo ok até aqui`)
+        contador = 1
+        while (contador <= 10){
+            var resultado = n_escolhido * contador
+            contador ++
+            tabuada_area.innerHTML += ` ${resultado} `
+        }
     }
 }
 
