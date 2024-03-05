f25c15efab0aaf892572b3d813472427a5befdf9
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-03-05 19:51:04 -0300

    feat: aprendendo Array

--- aula15/script.js
+++ aula15/script.js
@@ -1,4 +1,19 @@
 var num = [5, 8, 2, 9, 3]
-num[5] = 6
-num.push(6)
-console.log (`${num}`)
+console.log (num)
+
+var ordem = 1
+for (contador = 0; contador <= 4; contador++){
+    console.log (`O ${ordem}º número é ${num[contador]}`)
+    ordem++
+}
+
+console.log (`${num.indexOf(8)}`)
+
+/*
+var contador = 0
+while (contador <= 4){
+    console.log (`Número ${num[contador]}`)
+    contador++
+
+}
+*/
