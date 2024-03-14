6338d6a34c2d9a66cc7e0f2bd84fc3655983c9d2
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-03-14 16:45:18 -0300

    feat: Aprendendo function

---
+++ aula16/funcao01.js
@@ -0,0 +1,11 @@
+function parimpar (n = 2){
+    if (n%2 == 0){
+        return 'Par!!'
+    } else {
+        return 'impar!!'
+
+    }                                
+}
+
+
+console.log (`${resposta}`)
