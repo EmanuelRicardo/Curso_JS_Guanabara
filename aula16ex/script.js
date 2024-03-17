f0aabb5c1c5f553c8ffa9feb9ce356cc89106183
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-03-16 21:34:23 -0300

    feat: js com função pro botão

---
+++ aula16ex/script.js
@@ -0,0 +1,7 @@
+function add_numero (){
+    var colecao_numeros = []
+    var numero_fornecido = window.document.getElementById ('numero_fornecido').value
+    
+    colecao_numeros.push(numero_fornecido)
+    window.alert (`${colecao_numeros}`)
+}
