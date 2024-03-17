8c626777034a677056998d0458fb907e4e6a24bf
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-03-17 12:51:58 -0300

    feat: Montando o array

--- aula16ex/script.js
+++ aula16ex/script.js
@@ -1,7 +1,16 @@
 function add_numero (){
-    var colecao_numeros = []
     var numero_fornecido = window.document.getElementById ('numero_fornecido').value
+    var local_resposta = window.document.getElementById ('local_resposta')
     
     colecao_numeros.push(numero_fornecido)
-    window.alert (`${colecao_numeros}`)
+    window.alert (colecao_numeros)
+    
 }
+var colecao_numeros = []
+var n_array = 0
+while (n_array < colecao_numeros.length){
+    
+}
+
+
+
