2c7a78201b19f3226d1de4c2bdf44f524e706b53
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-03-17 13:38:01 -0300

    feat: contador pro array pra ir lançando os valores

--- aula16ex/script.js
+++ aula16ex/script.js
@@ -1,16 +1,26 @@
+var local_resposta = window.document.getElementById ('local_resposta')
+var colecao_numeros = []
+
 function add_numero (){
+    // Pegar os números digitados, colocar no aray e mandar em forma de alerta no site
     var numero_fornecido = window.document.getElementById ('numero_fornecido').value
-    var local_resposta = window.document.getElementById ('local_resposta')
     
     colecao_numeros.push(numero_fornecido)
     window.alert (colecao_numeros)
+
+    var opcoes = window.document.createElement('option')
+    var contador_array = 0
+    while (contador_array < colecao_numeros.length){
+        opcoes.text = `O número é: ${colecao_numeros[contador_array]}`
+        local_resposta.appendChild(opcoes)
+        contador_array++
+    }
+    
     
 }
-var colecao_numeros = []
-var n_array = 0
-while (n_array < colecao_numeros.length){
-    
-}
+
+
+
 
 
 
